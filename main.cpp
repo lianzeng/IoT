b8e0503651384f5bdaf08789731481a5aafda213
ZL <zenliang@foxmail.com>
2018-04-08 21:26:36 +0800

    Update main.cpp
    1)read input from file instead of console; 2)add timerfd to periodically send special cmd

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//usage:  ./OBD 2>&1 | tee result.txt;  or  ./OBD 1> result.txt;
+//usage: sudo  ./OBD 2>&1 | tee result.txt;  or  ./OBD 1> result.txt;
 
 
 #include <stdio.h>
@@ -8,12 +8,15 @@
 #include <termio.h>
 #include <cstring>
 #include <errno.h>
+#include <sys/timerfd.h>
+#include <stdint.h>
+#include <sys/time.h>
 
 
-//typedef unsigned int  speed_t;
 
 extern int init_tty(int fd, const speed_t baudrate);
 
+static char obditem[1024] = {0};
 
 int openSerialPort(const char* portName, const speed_t baudrate)
 {
@@ -26,7 +29,7 @@ int openSerialPort(const char* portName, const speed_t baudrate)
         return -1;
     }
 
-    printf("open %s ok!\n",portName);
+    printf("open %s ok!,ttyfd = %d \n",portName, fd);
 
     if(init_tty(fd, baudrate) == -1)//初始化串口
     {
@@ -95,7 +98,7 @@ int activeObd(int fd)
             printf("active obd failure \n");
             continue;
         } else {
-            printf("active obd success. \n");
+            printf("active obd success. \n\n");
             return 0;
         }
     }
@@ -103,21 +106,29 @@ int activeObd(int fd)
     return -1;
 }
 
-void readAndSendUserCmd(const int cmdfd, const int ttyfd)
+void readAndSendUserCmd(const char* obdfile, const int ttyfd)
 {
-    char cmd[100] = {0};
-    int len = read(cmdfd, cmd, sizeof(cmd) - 1);
+    int cmdfd = open(obdfile, O_RDONLY);
+    if(cmdfd < 0)
+    {
+        printf("open %s failed\n",obdfile);
+        return;
+    }
+    printf("open %s ok, fd = %d \n",obdfile, cmdfd);
+
+    int len = read(cmdfd, obditem, sizeof(obditem) - 1);
     if(len > 0)
     {
-        printf("send user cmd: %s \n", cmd);
-        if(!strncmp(cmd, "QUIT",4) || !strncmp(cmd, "quit",4))
-            exit(0);
-        int wrotebytes = write(ttyfd, cmd, len);
+        printf("ready to read  obditem: %s \n", obditem);
+
+        int wrotebytes = write(ttyfd, obditem, len);
         if(wrotebytes != len)
-            printf("send cmd failed \n");
+            printf("send obditem %d bytes,but expect %d bytes \n",wrotebytes, len);
     }
     else
-        printf("read user cmd failed, len= %d \n", len);
+        printf("%s is empty, readlen= %d \n",obdfile, len);
+
+    close(cmdfd);
 }
 
 void receiveObdData(const int fd)
@@ -130,25 +141,53 @@ void receiveObdData(const int fd)
     }
 }
 
-void fetchObdData(const int fd, const int cmdfd)
+void sendFaultCmd(int ttyfd)
 {
-    //fetch obd data according to user cmd
+    const char* dtc = "AT+DTC";
+    int wrotebytes = write(ttyfd, dtc, strlen(dtc));
+    printf("send cmd: %s \n", dtc);
+    if(wrotebytes != strlen(dtc))
+        printf("Error on send Fault cmd \n");
+#if 0
+    struct timeval now;
+    if(!gettimeofday(&now, NULL))
+    {
+        printf("time=%ld.%06ld \n",now.tv_sec,now.tv_usec);
+    }
+#endif
+}
+
+void fetchObdData(const int fd, const int timerfd)
+{//fetch obd data according to user cmd defined in obditem.txt
+
+    static bool dataFaultSwitch = 1;
     fd_set readset;
-    int maxfd = fd > cmdfd ? (fd + 1) : (cmdfd + 1);
+    int maxfdp1 = fd > timerfd ? (fd + 1) : (timerfd + 1);
     int result;
     do{
         FD_ZERO(&readset);
         FD_SET(fd, &readset);
-        FD_SET(cmdfd,&readset);
-        result = select(maxfd, &readset, NULL, NULL, NULL);
+        FD_SET(timerfd, &readset);
+        result = select(maxfdp1, &readset, NULL, NULL, NULL);
     }while(result == -1 && errno == EINTR);
 
     if(result > 0)
     {
-        if(FD_ISSET(cmdfd,&readset))
+        //printf("num of %d fds are ready.\n", result);
+        if(FD_ISSET(timerfd,&readset))
         {
-            readAndSendUserCmd(cmdfd, fd);
-            //tcflush(fd, TCIFLUSH);
+            uint64_t dummy;
+            read(timerfd, &dummy, sizeof(dummy));
+            if(dataFaultSwitch)
+            {
+                sendFaultCmd(fd);//periodically send AT+DTC to detect fault.
+            }
+            else
+            {
+                write(fd, obditem, strlen(obditem));
+                printf("sending %s \n", obditem);
+            }
+            dataFaultSwitch ^= 0x1;
         }
         if(FD_ISSET(fd, &readset))
             receiveObdData(fd);
@@ -156,32 +195,55 @@ void fetchObdData(const int fd, const int cmdfd)
     else if(result < 0)
     {
         printf("Error on select(): %s \n", strerror(errno));
-        exit(1);
+        //exit(1);
     }
     else
     {
-        printf("error:select return 0 \n");//shoud not reach since select(timeout=infinite)
+        printf("Error:select return 0 \n");//shoud not reach since select(timeout=infinite)
+    }
+}
+
+int createObdFaultFd(const int fault_period)
+{
+    int faultTimerfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+    if(faultTimerfd < 0) {
+        perror("timerfd_create failed");
+        return -1;
     }
 
+    struct itimerspec tmr;
+    tmr.it_value.tv_sec = fault_period;
+    tmr.it_value.tv_nsec = 0;
+    tmr.it_interval.tv_sec = fault_period;
+    tmr.it_interval.tv_nsec = 0;
+    timerfd_settime(faultTimerfd, 0, &tmr, NULL);
+
+    printf("faultfd = %d \n",faultTimerfd);
+    return faultTimerfd;
 }
 
 int main(int argc, char* argv[])
 {
     const char* serialPort = "/dev/ttyUSB0";
 
-    speed_t baudrate = (argc > 1) ? atoi(argv[1]) : 115200;
+    const speed_t baudrate = 115200;//(argc > 1) ? atoi(argv[1]) : 115200;
 
-    int fd = openSerialPort(serialPort, baudrate);
-    if(fd < 0) return -1;
+    int ttyfd = openSerialPort(serialPort, baudrate);
+    if(ttyfd < 0) return -1;
 
-    if(activeObd(fd) < 0) return -1;
+    if(activeObd(ttyfd) < 0) return -1;
+
+
+    readAndSendUserCmd("obditem.txt", ttyfd);
+
+    int faultTimerfd = createObdFaultFd(2);//period=2second(at least!)
 
     while(true)
     {
-        fetchObdData(fd, STDIN_FILENO);
+        fetchObdData(ttyfd, faultTimerfd);
     }
 
-    close(fd);
+    close(ttyfd);
     return 0;
 }
 
