ae4c41134ef9c54887dbeb07a3c5ea5240fd1f2a
ZL <zenliang@foxmail.com>
2018-07-23 14:58:53 +0800

    Update unixIPC.cpp
    unixIPC server side refer to: https://github.com/lianzeng/wakaama/blob/master/examples/client/unixIPC.c

--- unixIPC.cpp
+++ unixIPC.cpp
@@ -1,5 +1,5 @@
 //
-// Created by impact on 18-7-4.
+// Created by zengliang on 18-7-4.
 //
 
 #include <sys/socket.h>
@@ -8,10 +8,74 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <cerrno>
+#include "reportJ1939.hpp"
 
-#define UNIX_IPC_NAME  "/var/lwm2mReport_ipc"      //filename must be absolute path, but not already exist
+
+#define UNIX_IPC_NAME  "/var/run/lwm2m.sock"      //filename must be absolute path, but not already exist
 #define CLIENT_NAME "obdclient"
 
+
+
+char* convertDataToJSON(ObdDataInfo* obdData)
+{
+
+    //JSON: {"10244":{"0":{"0":"running","1":"160.00","2":"800.00","3":"1531210007","4":"1 : P1012 ","5":"75","6":"0.60","7":"30"}}}
+
+    const int VEHICLE_OBJECT_ID = 10244;
+
+
+    static char output[200];
+
+    int len = 0;
+    len += sprintf(output+len,"{");
+    len += sprintf(output+len,"\"%d\":",VEHICLE_OBJECT_ID);//objId
+
+    len += sprintf(output+len, "{\"0\":{"); //instanceid
+
+    len += sprintf(output+len, "\"0\":"); //resourceId = 0
+    //const char* state = (obdData->vehicleSpeed > 0) ? "running" : "stopped";
+    len += sprintf(output+len, "\"%s\"", obdData->state);
+
+    len += sprintf(output+len,",\"1\":");
+    len += sprintf(output+len,"\"%.2f\"",obdData->vehicleSpeed);
+
+    len += sprintf(output+len,",\"2\":");
+    len += sprintf(output+len,"\"%.2f\"",obdData->engSpeed);
+
+    len += sprintf(output+len,",\"3\":");
+    len += sprintf(output+len,"\"%ld\"",obdData->timestamp);
+
+    len += sprintf(output+len,",\"4\":");
+    len += sprintf(output+len,"\"%d : %s\"",obdData->faultNum,obdData->falutDetail);
+
+    len += sprintf(output+len,",\"5\":");
+    len += sprintf(output+len,"\"%d\"",obdData->engLoad);
+
+    len += sprintf(output+len,",\"6\":");
+    len += sprintf(output+len,"\"%.2f\"",obdData->fuelLevelInput);
+
+    len += sprintf(output+len,",\"7\":");
+    len += sprintf(output+len,"\"%d\"",obdData->engCoolanTemperature);
+
+    len += sprintf(output+len,",\"8\":");
+    len += sprintf(output+len,"\"%.4f\"",obdData->totalVehicleHours);
+
+    len += sprintf(output+len,",\"9\":");
+    len += sprintf(output+len,"\"%.3f\"",obdData->totalDistance);
+
+    len += sprintf(output+len,",\"10\":");
+    len += sprintf(output+len,"\"%.2f\"",obdData->batteryVoltage);//not found for ISO15765
+
+    len += sprintf(output+len,",\"11\":");
+    len += sprintf(output+len,"\"%d\"",obdData->emergeBrake);//emerge brake
+
+    len += sprintf(output+len, "}}}");
+
+    output[len] = 0;
+
+    printf("\n JSON: %s \n", output);
+    return output;
+}
 
 int createUnixSocket()
 {
@@ -40,25 +104,32 @@ int createUnixSocket()
 }
 
 
-void sendIpcData(int fd)
+void send_Dgram(const int ipcfd, const char* buffer)
 {
-    const char* buffer = "{objId:10244,instId:0,resNum:4,resValues:[0:1,1:100,2:800,3:1234567]}";
-
-
     struct sockaddr_un server;
     memset(&server, 0, sizeof(server));
-
     server.sun_family = AF_UNIX;
     strcpy(server.sun_path, UNIX_IPC_NAME);
 
-
-    int numBytes = sendto(fd, buffer, strlen(buffer), 0, (struct sockaddr *)&server, sizeof(server));
+    //even if lwm2mclient(udp server) not run, this program still ok.
+    int numBytes = sendto(ipcfd, buffer, strlen(buffer), 0, (struct sockaddr *)&server, sizeof(server));
 
     if(numBytes != strlen(buffer))
     {
-           printf("sendto lwm2mclient error=%d : %s \n",errno,  strerror(errno));
+        printf("sendto lwm2mclient error=%d : %s \n",errno,  strerror(errno));
     }
     else
         printf("sendto lwm2mclient ok.\n");
+}
+
+void sendIpcData(const int ipcfd, ObdDataInfo* obdData)
+{
+    //const char* buffer = "{\"10244\":{\"0\":{\"0\":\"running\",\"1\":\"800\",\"2\":\"300\",\"3\":\"123456789ABCD\"}}}";
+    const char* buffer = convertDataToJSON(obdData);
+    send_Dgram(ipcfd, buffer);
 
 }
+
+
+
+
